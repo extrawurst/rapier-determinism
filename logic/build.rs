4b2798800a55c70736775a6232fd753f3695af04
Stephan Dilly <dilly.stephan@gmail.com>
2021-02-03 23:46:37 +0100

    initial commit

---
+++ logic/build.rs
@@ -0,0 +1,13 @@
+use gdnative_project_utils::*;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // directory to scan for Rust files
+    let classes = scan_crate("src")?;
+
+    // generate files inside the Godot project directory
+    Generator::new()
+        .godot_project_dir("../godot")
+        .build(classes)?;
+
+    Ok(())
+}
